be4321bf21dcce2e12fbbf5f777719200e2fb38c
league <league@imac-24.attlocal.net>
2019-11-16 17:23:48 -0800

    Code

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -58,14 +58,40 @@ public class ToDoList implements ActionListener{
 		load.addActionListener(this);
 		frame.pack();
 		frame.setVisible(true);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/_03_To_Do_List/newFile.txt"));
+			String line = br.readLine();
+			while(line != null){
+				if(line.length() < 4) {
+					continue;
+				}
+				
+				System.out.println(line);
+				tasks.add(line.substring(3));
+				line = br.readLine();
+			}
+			
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
+	String createMessage() {
 		String message = "";
 		for (int i = 0; i < tasks.size(); i++) {
 			message = message + (i+1) + ". " + tasks.get(i) + "\n";
 		}
+		return message;
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String message = createMessage();
+		
 		// TODO Auto-generated method stub
 		if(e.getSource() == add) {
 			String task = JOptionPane.showInputDialog(panel, "Give a task");
@@ -79,7 +105,40 @@ public class ToDoList implements ActionListener{
 			tasks.remove(Integer.parseInt(in) - 1);
 		}
 		if(e.getSource() == save) {
-			
+			try {
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/newFile.txt");
+				fw.write(message);
+				fw.close();
+			} catch (IOException d) {
+				d.printStackTrace();
+			}
+		}
+		if(e.getSource() == load) {
+			String fileName = "";
+			JFileChooser jfc = new JFileChooser();
+			int returnVal = jfc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				fileName = jfc.getSelectedFile().getAbsolutePath();
+			}
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(fileName));
+				String line = br.readLine();
+				tasks.clear();
+				while(line != null){
+					System.out.println(line);
+					tasks.add(line.substring(3));
+					line = br.readLine();
+				}
+				
+				br.close();
+				message = createMessage();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
 		}
 	}
 	
